a340a5d9a596f6ae583a7818ac68c9f4a673ed61
ronick-grammer <73280175+ronick-grammer@users.noreply.github.com>
2020-12-03 15:13:53 +0900

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main(void){
-    int array[10] = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+    int array[10] = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8}; 
     
     for(int i = 0; i < 10; i++){
-        int min_index = i;
-        for(int j = i+1; j < 10; j++){
-            if(array[j] < array[min_index]){
+        int min_index = i; //최소값 인덱스
+        for(int j = i+1; j < 10; j++){ 
+            if(array[j] < array[min_index]){ //하나하나씩 비교하여 최소값 인덱스 재설정
                 min_index = j;
             }
         }
-        swap(array[i], array[min_index]);
+        swap(array[i], array[min_index]); // 스왑
     }
 
     for(int i = 0; i < 10; i++){
