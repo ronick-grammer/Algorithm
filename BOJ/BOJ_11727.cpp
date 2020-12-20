5650e5a57e22358388e8c1dba5fb14634c0038c3
ronick-grammer <73280175+ronick-grammer@users.noreply.github.com>
2020-12-20 17:15:59 +0900

    Create BOJ_11727.cpp

---
+++ BOJ/BOJ_11727.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001];
+
+int main(void){
+    int n;
+    cin>>n;
+
+    dp[1] = 1; // 2x1 일때 는 한가지 방법뿐
+    dp[2] = 3; // 2x2 일때는 세가지 방법
+    for(int i = 3; i <= n; i++){
+        dp[i] = dp[i-1] + dp[i-2] * 2; // 점화식 d(n-1) + d(n-2) * 2
+        dp[i] %=  10007;
+    }
+
+    cout<<dp[n]<<endl;
+}
