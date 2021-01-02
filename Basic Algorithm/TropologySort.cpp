179cf24ee26c6e09c26dd995e5eafd7159eeb158
ronick-grammer <73280175+ronick-grammer@users.noreply.github.com>
2021-01-02 17:13:19 +0900

    Update TropologySort.cpp

--- Basic Algorithm/TropologySort.cpp
+++ Basic Algorithm/TropologySort.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // 시간 복잡도는 O(V + E) 이다. 수행을 할때 모든 간선들을 확인하면서 노드의 차수를 확인하기 
 
 int n, m; // n: 노드 갯수, m: 간선 갯수
-int degree[100001]; // 각 노드의 진입차수
+int inDegree[100001]; // 각 노드의 진입차수
 vector<int> v[100001];  
 vector<int> sorted; // 위상정렬된 변수
 queue<int> q; 
@@ -21,8 +21,8 @@ void tropologySort(){ // 위상 정렬
         for(int i = 0; i < v[index].size(); i++){ // 연결된 노드의 수만큼 반복
             int x = v[index][i]; // 노드 반환
             
-            degree[x]--; // 해당 노드의 진입차수를 하나씩 감소시킨다
-            if(degree[x] <= 0){  // 진입 차수가 0 이거나 보다 작으면
+            inDegree[x]--; // 해당 노드의 진입차수를 하나씩 감소시킨다
+            if(inDegree[x] <= 0){  // 진입 차수가 0 이거나 보다 작으면
                 q.push(x);       // 큐에 넣고
                 sorted.push_back(x); // 순서대로 넣어 정렬한다
             }            
@@ -38,11 +38,11 @@ int main(void){
         int from, to;  // from 에서 to 의 방향
         cin >> from >> to;
         v[from].push_back(to);
-        degree[to]++; // 해당 노드의 진입차수를 하나씩 증가시킨다
+        inDegree[to]++; // 해당 노드의 진입차수를 하나씩 증가시킨다
     }
 
     for(int i = 1; i <= n; i++){
-        if(degree[i] == 0){ // 진입차수가 0인 노드들을 시작으로한다
+        if(inDegree[i] == 0){ // 진입차수가 0인 노드들을 시작으로한다
             q.push(i);
             sorted.push_back(i);
         }
