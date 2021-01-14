93670647b8fe4a344c1241b01d10673e50e2d519
Ronick <73280175+ronick-grammer@users.noreply.github.com>
2021-01-14 18:33:35 +0900

    Update AdditionOrMultiplication_p312.cpp

--- This Is Coding Test/AdditionOrMultiplication_p312.cpp
+++ This Is Coding Test/AdditionOrMultiplication_p312.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
+#include <vector>
 #include <string>
 using namespace std;
 
 string s; // 숫자를 담을 문자열
-int sum[20]; // 문자열을 숫자로 변환할 정수배열
-int result; // 최대 결과를 담을 변수
+vector<int> v; // 문자열을 숫자로 변환할 정수배열
+int sum; // 최대 결과를 담을 변수
 
 int main(void){
     cin>>s;
 
     for(int i = 0; i < s.size(); i++){
-        sum[i] = s[i] - '0'; // 문자열의 각 문자를 숫자로 변환
+        int x = s[i] - '0'; // 문자열의 각 문자를 숫자로 변환
+        v.push_back(x);
     }
 
-    result = sum[0]; 
-    for(int i = 1; i < s.size(); i++){ // 숫자들의 갯수만큼 반복
-        
-        if(result <= 1){ // 1보다 작거나 같으면 더한다. 1 이하인 수를 곱한 것보다 더하는 것이 합이 더 커지기 때문이다
-            result = result + sum[i];
+    if(v[0] <= 1 || v[1] <= 1){ // 가장 맨 앞의 두 숫자중 하나라도 1 이하이면 더한다
+        sum += v[0] + v[1];
+    }
+    else{ // 2 이상이면 곱한다
+        sum += v[0] * v[1];
+    }
+    
+    for(int i = 2; i < v.size(); i++){ // 위에서 앞의 두 숫자를 처리하였으므로 세번째 숫자부터 진행한다
+        if(v[i] <= 1){ // 1 이하이면 더하는 것이 더 크다
+            sum += v[i];
         }
-        else{    // 1보다 크면 곱한다. 2이상인 수를 더하는 것보다 곱하는 것이 합이 더 커지기 때문이다
-            result = result * sum[i];
-        }
+        else{ // 2 이상이면 곱하는 것이 더 크다
+            sum *= v[i];
+        }        
     }
 
-    cout<<result<<endl;
+    cout<<sum<<endl;
 }
