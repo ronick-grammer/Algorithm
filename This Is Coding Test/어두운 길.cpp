21ea167c46e2cc353a050bf0480afd95372ed536
Ronick <73280175+ronick-grammer@users.noreply.github.com>
2021-02-28 19:05:17 +0900

    Create 어두운 길.cpp

---
+++ This Is Coding Test/어두운 길.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+using namespace std;
+
+int n, m;
+int parent[200001];
+vector<pair<int, pair<int, int> > > graph; 
+
+bool compare(pair<int, pair<int, int> > a, pair<int, pair<int, int> > b){
+    return a.second.second < b.second.second;
+}
+
+int findParent(int x){
+    if(parent[x] == x){
+        return x;
+    }
+    else{
+        return parent[x] = findParent(parent[x]);
+    }
+}
+
+void unionParent(int a, int b){
+    int parent_a = findParent(a);
+    int parent_b = findParent(b);
+
+    if(parent_a < parent_b){
+        parent[parent_b] = parent_a;
+    }
+    else{
+        parent[parent_a] = parent_b;
+    }
+}
+
+int main(void){
+    cin >> n >> m;
+
+    for(int i = 0; i < m; i++){
+        int a, b, cost;
+        cin >> a >> b >> cost;
+
+        graph.push_back(make_pair(a, make_pair(b, cost)));
+    }
+
+    for(int i = 0; i < n; i++){
+        parent[i] = i; // 자기 자신에 속하도록 초기화
+    }
+
+    sort(graph.begin(), graph.end(), compare); // 최소 비용만으로 그래프를 이루기 위하여 정렬
+
+    // 크루스칼 알고리즘 ㄱㄱ
+    int result = 0;
+    for(int i = 0; i < m; i++){
+        int a = graph[i].first;
+        int b = graph[i].second.first;
+        int cost = graph[i].second.second;
+
+        // 아낄 수 있는 최대 금액 구하기
+        if(findParent(a) == findParent(b)){ // 두 마을이 이미 같은 그룹이라면(사이클을 이룬다면) 그 비용을 더해준다
+            result += cost;
+        }
+        else{
+            unionParent(a, b); // 그게 아니면 두 마을을 하나의 그룹으로 합친다
+        }
+    }
+
+    cout<<result<<'\n';
+}
