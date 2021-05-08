188100c7154284641a2a3b433378956c128d09c9
Ronick <73280175+ronick-grammer@users.noreply.github.com>
2021-05-08 19:52:48 +0900

    Create 어느코테_KXXXX_01.cpp

---
+++ etc/어느코테_KXXXX_01.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+// 테스트 케이스
+"one4seveneight"  ->	1478
+"23four5six7"	->   234567
+"2three45sixseven" ->	234567
+"123" ->	123
+*/
+string numbers[10] = {"zero", "one", "two", "three", "four", 
+                     "five", "six", "seven", "eight", "nine"};
+
+string translate(string s){
+    if(s == "zero")
+        return "0";
+    if(s == "one")
+        return "1";
+    if(s == "two")
+        return "2";
+    if(s == "three")
+        return "3";
+    if(s == "four")
+        return "4";
+    if(s == "five")
+        return "5";
+    if(s == "six")
+        return "6";
+    if(s == "seven")
+        return "7";
+    if(s == "eight")
+        return "8";
+    if(s == "nine")
+        return "9";
+
+    return "";
+}
+
+int solution(string s) {
+    int answer = 0;
+
+    for(int j = 0; j < 10; j++){
+        int pos = 0;
+        while((pos = s.find(numbers[j], pos)) != string::npos){
+            s.replace(pos, numbers[j].size(), translate(numbers[j]));
+            pos++; // 인덱스 1 증가
+        }
+     }
+    answer = stoi(s);
+    return answer;
+}
