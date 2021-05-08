aff26148df5cbba279427a8d5256a304c8949fb0
Ronick <73280175+ronick-grammer@users.noreply.github.com>
2021-05-09 00:33:58 +0900

    Update 어느코테_KXXXX_01.cpp

--- etc/어느코테_KXXXX_01.cpp
+++ etc/어느코테_KXXXX_01.cpp
@@ -14,38 +14,13 @@ using namespace std;
 string numbers[10] = {"zero", "one", "two", "three", "four", 
                      "five", "six", "seven", "eight", "nine"};
 
-string translate(string s){
-    if(s == "zero")
-        return "0";
-    if(s == "one")
-        return "1";
-    if(s == "two")
-        return "2";
-    if(s == "three")
-        return "3";
-    if(s == "four")
-        return "4";
-    if(s == "five")
-        return "5";
-    if(s == "six")
-        return "6";
-    if(s == "seven")
-        return "7";
-    if(s == "eight")
-        return "8";
-    if(s == "nine")
-        return "9";
-
-    return "";
-}
-
 int solution(string s) {
     int answer = 0;
 
     for(int j = 0; j < 10; j++){
         int pos = 0;
         while((pos = s.find(numbers[j], pos)) != string::npos){
-            s.replace(pos, numbers[j].size(), translate(numbers[j]));
+            s.replace(pos, numbers[j].size(), to_string(j));
             pos++; // 인덱스 1 증가
         }
      }
