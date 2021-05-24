45c06bd2009336cf31182926d0036a06b013ae7d
Ronick <73280175+ronick-grammer@users.noreply.github.com>
2021-05-24 22:30:22 +0900

    Update BOJ_1912_연속합.cpp

--- BOJ/BOJ_1912_연속합.cpp
+++ BOJ/BOJ_1912_연속합.cpp
@@ -17,8 +17,8 @@ int main(void){
     dp[1] = nums[1];
     for(int i = 2; i <= n; i++){
         // "단, 수는 한 개 이상 선택해야 한다." 의 지문에 의하면 자기 자신도 비교하여야 한다.
-        // dp[i] = max(자기 자신, 연속해서 계속 더해온 수 + 자기 자신, 바로 직전 수 + 자기 자신)
-        dp[i] = max(nums[i], max(dp[i-1] + nums[i], nums[i-1] + nums[i]));
+        // dp[i] = max(연속해서 계속 더해온 수 + 자기 자신, 자기 자신)
+        dp[i] = max(dp[i-1] + nums[i], nums[i]);
         result = max(result, dp[i]);
     }
 
