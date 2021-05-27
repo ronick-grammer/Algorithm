3469e6c6d256a9337560ce465f11a082162d7828
Ronick <73280175+ronick-grammer@users.noreply.github.com>
2021-05-27 22:02:02 +0900

    Create BOJ_1309_동물원_풀이_1.cpp

---
+++ BOJ/BOJ_1309_동물원_풀이_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int dp[100001];
+
+int main(void){
+    int n;
+    cin >> n;
+
+    dp[0] = 1;
+    dp[1] = 3;
+    for(int i = 2; i <= n; i++){
+        dp[i] = (dp[i-1] * 2 + dp[i-2]) % 9901;
+    }
+
+    cout<<dp[n]<<'\n';
+}
