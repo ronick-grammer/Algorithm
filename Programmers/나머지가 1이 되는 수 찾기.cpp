40c66cf34d77b51a9248a601a88269d4e5a0f819
Ronick <73280175+ronick-grammer@users.noreply.github.com>
2021-11-04 20:37:25 +0900

    Create 나머지가 1이 되는 수 찾기.cpp

---
+++ Programmers/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,18 @@
+//problem link: https://programmers.co.kr/learn/courses/30/lessons/87389
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int i = 2; i <= n; i++) {
+        if(n % i == 1) {
+            answer = i;
+            break;
+        }
+    }
+    
+    return answer;
+}
