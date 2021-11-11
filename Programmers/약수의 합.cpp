e08a0b3da7dbda6c8b3a7f40924cde6220e254db
Ronick <73280175+ronick-grammer@users.noreply.github.com>
2021-11-11 19:53:15 +0900

    Create 약수의 합.cpp

---
+++ Programmers/약수의 합.cpp
@@ -0,0 +1,16 @@
+//problem link: https://programmers.co.kr/learn/courses/30/lessons/12928
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    // 참 쉽죠이
+    for(int i = 1; i <= n; i++) {
+        if(n % i  == 0) answer += i;
+    }
+    
+    return answer;
+}
