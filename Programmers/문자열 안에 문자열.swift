6d9b93d60e95da627c8f22465ade8f96d7b592d2
Ronick <73280175+ronick-grammer@users.noreply.github.com>
2023-02-12 18:17:57 +0900

    Create 문자열 안에 문자열.swift

---
+++ Programmers/문자열 안에 문자열.swift
@@ -0,0 +1,7 @@
+//problem link: https://school.programmers.co.kr/learn/courses/30/lessons/120908
+import Foundation
+
+func solution(_ str1:String, _ str2:String) -> Int {
+    
+    return str1.contains(str2) ? 1 : 2
+}
